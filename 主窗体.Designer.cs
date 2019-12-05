fc0df1ee910356e708fae3ad8ba1d731327a5924
wxp <wxp@zlsoft.cn>
2019-12-06 03:03:33 +0800

    首次提交代码

---
+++ 主窗体.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace 配置IP地址
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.B_execute = new System.Windows.Forms.Button();
+            this.In_IP = new System.Windows.Forms.TextBox();
+            this.文本_IP地址 = new System.Windows.Forms.Label();
+            this.AutoIP = new System.Windows.Forms.RadioButton();
+            this.操作模式 = new System.Windows.Forms.GroupBox();
+            this.SetIP = new System.Windows.Forms.RadioButton();
+            this.IP配置 = new System.Windows.Forms.GroupBox();
+            this.In_默认网关 = new System.Windows.Forms.TextBox();
+            this.文本_默认网关 = new System.Windows.Forms.Label();
+            this.In_子网掩码 = new System.Windows.Forms.TextBox();
+            this.文本_子网掩码 = new System.Windows.Forms.Label();
+            this.按钮_测试 = new System.Windows.Forms.Button();
+            this.IP_Now = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.TextBox();
+            this.文本_执行结果 = new System.Windows.Forms.Label();
+            this.文本_适配器 = new System.Windows.Forms.Label();
+            this.适配器列表 = new System.Windows.Forms.ComboBox();
+            this.操作模式.SuspendLayout();
+            this.IP配置.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // B_execute
+            // 
+            this.B_execute.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.B_execute.Location = new System.Drawing.Point(28, 393);
+            this.B_execute.Name = "B_execute";
+            this.B_execute.Size = new System.Drawing.Size(151, 35);
+            this.B_execute.TabIndex = 0;
+            this.B_execute.Text = "更改";
+            this.B_execute.UseVisualStyleBackColor = true;
+            this.B_execute.Click += new System.EventHandler(this.B_execute_Click);
+            // 
+            // In_IP
+            // 
+            this.In_IP.Location = new System.Drawing.Point(133, 18);
+            this.In_IP.Name = "In_IP";
+            this.In_IP.Size = new System.Drawing.Size(180, 21);
+            this.In_IP.TabIndex = 1;
+            // 
+            // 文本_IP地址
+            // 
+            this.文本_IP地址.AutoSize = true;
+            this.文本_IP地址.Location = new System.Drawing.Point(20, 24);
+            this.文本_IP地址.Name = "文本_IP地址";
+            this.文本_IP地址.Size = new System.Drawing.Size(41, 12);
+            this.文本_IP地址.TabIndex = 2;
+            this.文本_IP地址.Text = "IP地址";
+            // 
+            // AutoIP
+            // 
+            this.AutoIP.AutoSize = true;
+            this.AutoIP.Location = new System.Drawing.Point(11, 29);
+            this.AutoIP.Name = "AutoIP";
+            this.AutoIP.Size = new System.Drawing.Size(107, 16);
+            this.AutoIP.TabIndex = 3;
+            this.AutoIP.TabStop = true;
+            this.AutoIP.Text = "自动获得IP地址";
+            this.AutoIP.UseVisualStyleBackColor = true;
+            // 
+            // 操作模式
+            // 
+            this.操作模式.Controls.Add(this.SetIP);
+            this.操作模式.Controls.Add(this.AutoIP);
+            this.操作模式.Location = new System.Drawing.Point(28, 140);
+            this.操作模式.Name = "操作模式";
+            this.操作模式.Size = new System.Drawing.Size(355, 104);
+            this.操作模式.TabIndex = 4;
+            this.操作模式.TabStop = false;
+            this.操作模式.Text = "操作模式";
+            // 
+            // SetIP
+            // 
+            this.SetIP.AutoSize = true;
+            this.SetIP.Location = new System.Drawing.Point(11, 61);
+            this.SetIP.Name = "SetIP";
+            this.SetIP.Size = new System.Drawing.Size(119, 16);
+            this.SetIP.TabIndex = 3;
+            this.SetIP.TabStop = true;
+            this.SetIP.Text = "使用下面的IP地址";
+            this.SetIP.UseVisualStyleBackColor = true;
+            // 
+            // IP配置
+            // 
+            this.IP配置.Controls.Add(this.In_默认网关);
+            this.IP配置.Controls.Add(this.文本_默认网关);
+            this.IP配置.Controls.Add(this.In_子网掩码);
+            this.IP配置.Controls.Add(this.文本_子网掩码);
+            this.IP配置.Controls.Add(this.In_IP);
+            this.IP配置.Controls.Add(this.文本_IP地址);
+            this.IP配置.Location = new System.Drawing.Point(28, 259);
+            this.IP配置.Name = "IP配置";
+            this.IP配置.Size = new System.Drawing.Size(355, 110);
+            this.IP配置.TabIndex = 5;
+            this.IP配置.TabStop = false;
+            this.IP配置.Text = "IP";
+            // 
+            // In_默认网关
+            // 
+            this.In_默认网关.Location = new System.Drawing.Point(133, 72);
+            this.In_默认网关.Name = "In_默认网关";
+            this.In_默认网关.Size = new System.Drawing.Size(180, 21);
+            this.In_默认网关.TabIndex = 1;
+            // 
+            // 文本_默认网关
+            // 
+            this.文本_默认网关.AutoSize = true;
+            this.文本_默认网关.Location = new System.Drawing.Point(20, 78);
+            this.文本_默认网关.Name = "文本_默认网关";
+            this.文本_默认网关.Size = new System.Drawing.Size(53, 12);
+            this.文本_默认网关.TabIndex = 2;
+            this.文本_默认网关.Text = "默认网关";
+            // 
+            // In_子网掩码
+            // 
+            this.In_子网掩码.Location = new System.Drawing.Point(133, 45);
+            this.In_子网掩码.Name = "In_子网掩码";
+            this.In_子网掩码.Size = new System.Drawing.Size(180, 21);
+            this.In_子网掩码.TabIndex = 1;
+            // 
+            // 文本_子网掩码
+            // 
+            this.文本_子网掩码.AutoSize = true;
+            this.文本_子网掩码.Location = new System.Drawing.Point(20, 51);
+            this.文本_子网掩码.Name = "文本_子网掩码";
+            this.文本_子网掩码.Size = new System.Drawing.Size(53, 12);
+            this.文本_子网掩码.TabIndex = 2;
+            this.文本_子网掩码.Text = "子网掩码";
+            // 
+            // 按钮_测试
+            // 
+            this.按钮_测试.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.按钮_测试.Location = new System.Drawing.Point(221, 393);
+            this.按钮_测试.Name = "按钮_测试";
+            this.按钮_测试.Size = new System.Drawing.Size(151, 35);
+            this.按钮_测试.TabIndex = 0;
+            this.按钮_测试.Text = "测试";
+            this.按钮_测试.UseVisualStyleBackColor = true;
+            this.按钮_测试.Click += new System.EventHandler(this.按钮_测试_Click);
+            // 
+            // IP_Now
+            // 
+            this.IP_Now.AutoSize = true;
+            this.IP_Now.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.IP_Now.Location = new System.Drawing.Point(33, 101);
+            this.IP_Now.Name = "IP_Now";
+            this.IP_Now.Size = new System.Drawing.Size(68, 14);
+            this.IP_Now.TabIndex = 6;
+            this.IP_Now.Text = "当前IP：";
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(417, 51);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.result.Size = new System.Drawing.Size(421, 376);
+            this.result.TabIndex = 7;
+            // 
+            // 文本_执行结果
+            // 
+            this.文本_执行结果.AutoSize = true;
+            this.文本_执行结果.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.文本_执行结果.Location = new System.Drawing.Point(415, 26);
+            this.文本_执行结果.Name = "文本_执行结果";
+            this.文本_执行结果.Size = new System.Drawing.Size(57, 12);
+            this.文本_执行结果.TabIndex = 8;
+            this.文本_执行结果.Text = "运行结果";
+            // 
+            // 文本_适配器
+            // 
+            this.文本_适配器.AutoSize = true;
+            this.文本_适配器.Location = new System.Drawing.Point(48, 54);
+            this.文本_适配器.Name = "文本_适配器";
+            this.文本_适配器.Size = new System.Drawing.Size(41, 12);
+            this.文本_适配器.TabIndex = 10;
+            this.文本_适配器.Text = "适配器";
+            // 
+            // 适配器列表
+            // 
+            this.适配器列表.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.适配器列表.FormattingEnabled = true;
+            this.适配器列表.Location = new System.Drawing.Point(97, 49);
+            this.适配器列表.Name = "适配器列表";
+            this.适配器列表.Size = new System.Drawing.Size(240, 20);
+            this.适配器列表.TabIndex = 11;
+            this.适配器列表.SelectedIndexChanged += new System.EventHandler(this.适配器列表_SelectedIndexChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 466);
+            this.Controls.Add(this.适配器列表);
+            this.Controls.Add(this.文本_适配器);
+            this.Controls.Add(this.文本_执行结果);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.IP_Now);
+            this.Controls.Add(this.IP配置);
+            this.Controls.Add(this.操作模式);
+            this.Controls.Add(this.按钮_测试);
+            this.Controls.Add(this.B_execute);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "IP&DNS配置";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.操作模式.ResumeLayout(false);
+            this.操作模式.PerformLayout();
+            this.IP配置.ResumeLayout(false);
+            this.IP配置.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B_execute;
+        private System.Windows.Forms.TextBox In_IP;
+        private System.Windows.Forms.Label 文本_IP地址;
+        private System.Windows.Forms.RadioButton AutoIP;
+        private System.Windows.Forms.GroupBox 操作模式;
+        private System.Windows.Forms.RadioButton SetIP;
+        private System.Windows.Forms.GroupBox IP配置;
+        private System.Windows.Forms.TextBox In_默认网关;
+        private System.Windows.Forms.Label 文本_默认网关;
+        private System.Windows.Forms.TextBox In_子网掩码;
+        private System.Windows.Forms.Label 文本_子网掩码;
+        private System.Windows.Forms.Button 按钮_测试;
+        private System.Windows.Forms.Label IP_Now;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label 文本_执行结果;
+        private System.Windows.Forms.Label 文本_适配器;
+        private System.Windows.Forms.ComboBox 适配器列表;
+    }
+}
+
---
+++ 主窗体.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 配置IP地址
+{
+    public partial class Main : Form
+    {
+        NetworkInterface[] adapters = null;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            //执行一次网卡查询
+            ExecuteInCmd("ipconfig");
+            //初始化适配器列表
+            adapters = NetworkInterface.GetAllNetworkInterfaces();
+            InitializeAdapterList();
+        }
+
+        public void InitializeAdapterList()
+        {
+            //获取适配器列表
+            适配器列表.DataSource = adapters.Select(n => n.Name).ToList();
+        }
+
+        
+
+        private void B_execute_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 按钮_测试_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public string GetLocalIP()
+        {
+            List<string> strIPs = new List<string>();
+            adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in adapters)
+            {
+
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+                    var mac = adapter.GetPhysicalAddress();
+                    Console.WriteLine(mac);
+                    IPInterfaceProperties ip = adapter.GetIPProperties();
+                    UnicastIPAddressInformationCollection ipCollection = ip.UnicastAddresses;
+                    foreach (UnicastIPAddressInformation ipadd in ipCollection)
+                    {
+                        //InterNetwork      IPV4地址      
+                        //InterNetworkV6    IPV6地址 
+                        if (ipadd.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {   //判断是否为ipv4
+                            Console.WriteLine(ipadd.Address.ToString());
+                            strIPs.Add(ipadd.Address.ToString());
+                        }
+                    }
+                }
+            }
+            return string.Join(";", strIPs);
+        }
+
+        /// <summary>
+        /// 切换适配器
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void 适配器列表_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //更新当前IP地址显示
+            ShowNowIp();
+            //加载适配器配置
+
+        }
+
+        /// <summary>
+        /// 显示当前IP地址
+        /// </summary>
+        private void ShowNowIp()
+        {
+            IP_Now.Text = "当前IP地址：";
+        }
+
+        private void SetThisAdapterClass(int index_in)
+        {
+            NowNet nowNet = new NowNet();
+            nowNet.type = null;
+            nowNet.index = index_in;
+            nowNet.name = null;
+            nowNet.ip_mode = null;
+            nowNet.ip = null;
+            nowNet.zwym = null;
+            nowNet.mrwg = null;
+        }
+
+        /// <summary>
+        /// 执行cmd命令
+        /// </summary>
+        /// <param name="cmdline"></param>
+        private void ExecuteInCmd(string cmdline)
+        {
+            if (cmdline != null)
+            {
+                using (var process = new Process())
+                {
+                    process.StartInfo.FileName = "cmd.exe";
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardInput = true;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.StartInfo.CreateNoWindow = true;
+
+                    process.Start();
+                    process.StandardInput.AutoFlush = true;
+                    process.StandardInput.WriteLine(cmdline + "&exit");
+
+                    //获取cmd窗口的输出信息  
+                    result.Text = process.StandardOutput.ReadToEnd();
+
+                    process.WaitForExit();
+                    process.Close();
+                }
+            }
+        }
+
+        class NowNet
+        {
+            /// <summary>
+            /// 连接方式：以太网；无线网络
+            /// </summary>
+            public string type { get; set; }
+            /// <summary>
+            /// 序号
+            /// </summary>
+            public int index { get; set; }
+            /// <summary>
+            /// 适配器名称
+            /// </summary>
+            public string name { get; set; }
+            /// <summary>
+            /// ip获取模式
+            /// </summary>
+            public string ip_mode { get; set; }
+            /// <summary>
+            /// IP地址
+            /// </summary>
+            public string ip { get; set; }
+            /// <summary>
+            /// 子网掩码
+            /// </summary>
+            public string zwym { get; set; }
+            /// <summary>
+            /// 默认网关
+            /// </summary>
+            public string mrwg { get; set; }
+        }
+    }
+}
